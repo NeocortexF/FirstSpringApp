eaad4079f71ec2da72dd1cc15005b18498e4c579
NeocortexF <eugeneshamkin@mail.ru>
2016-11-12 18:58:50 +0500

    Adding new file of FirstSpringApp

---
+++ FirstSpringApp/src/main/java/com/yet/spring/core/App.java
@@ -0,0 +1,47 @@
+package com.yet.spring.core;
+
+import com.sun.glass.ui.Application;
+import com.yet.spring.core.beans.Client;
+import com.yet.spring.core.loggers.ConsoleEventLogger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    Client client;
+    ConsoleEventLogger eventLogger;
+
+    public App() {
+    }
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+        //когда запрашиваешь бин по id то нужно есть кастить к приводимому классу
+        //когда вызываешь бин по типу и id то кастить не придется благодаря дженерикам
+        App app = (App) ctx.getBean("app");
+
+        app.logEvent("Some event for 1");
+        app.logEvent("Some event for 2");
+    }
+
+// вместо этого
+//        app.client = new Client("1", "John Smith");
+//        app.eventLogger = new ConsoleEventLogger();
+
+
+
+// это!
+//    public App(Client client, ConsoleEventLogger eventLogger) {
+//        this.client = client;
+//        this.eventLogger = eventLogger;
+//    }
+   // app.logEvent("some event for user 1");
+
+    private void logEvent(String msg) {
+        String message = msg.replaceAll(
+                client.getId(), client.getFullName());
+        eventLogger.logEvent(message);
+
+    }
+
+}
